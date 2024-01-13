afffff3871e9c7c5b97818f59ac1d3f58a9ce735
drsky <contact@drsky.ovh>
2024-01-13 12:56:16 +0100

    feat: Implement navigation elements data structure
    - Define navigationElements as a constant array of type DefaultNavItems.
    - Include navigation items for Home and List Employees with corresponding paths.

---
+++ src/constants/navigation.ts
@@ -0,0 +1,16 @@
+import { DefaultNavItems } from "../Types/navigation";
+
+const navigationElements: DefaultNavItems = [
+  {
+    id: 1,
+    label: "Home",
+    path: "/",
+  },
+  {
+    id: 2,
+    label: "List Employees",
+    path: "/employee",
+  },
+];
+
+export default navigationElements;
