22e8663d9740c49e67946fcdbfb5379bd14e0975
drsky <contact@drsky.ovh>
2024-01-13 12:42:10 +0100

    feat(App): Integrate React Router for navigation
    - Replace Welcome component with React Router setup in App.tsx.
    - Add routes for Home, Employee, and PageNotFound pages, nested under a Root layout.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,25 @@
-import Welcome from "@components/Welcome/Welcome";
+import {
+  Route,
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from "react-router-dom";
+import Root from "@layouts/Root/root";
+import Home from "@pages/home/home";
+import Employee from "@pages/employee/employee";
+import PageNotFound from "@pages/PageNotFound/PageNotFound";
 
 function App() {
-  return <Welcome />;
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path='/' element={<Root />}>
+        <Route index element={<Home />} />
+        <Route path='employee' element={<Employee />} />
+        <Route path='*' element={<PageNotFound />} />
+      </Route>
+    )
+  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
