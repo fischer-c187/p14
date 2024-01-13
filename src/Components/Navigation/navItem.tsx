3808ea297e0ab52611bc4ff1b7ec67c7cd2dd68f
drsky <contact@drsky.ovh>
2024-01-13 12:44:33 +0100

    feat: Implement NavItem component
    - Develop and integrate the NavItem component for use in navigation menus.

---
+++ src/Components/Navigation/navItem.tsx
@@ -0,0 +1,27 @@
+import { NavLink } from "react-router-dom";
+import { twMerge } from "tailwind-merge";
+import { DefaultNavItem } from "../../Types/navigation";
+
+type NavItemProps = {
+  item: DefaultNavItem;
+};
+
+function NavItem({ item }: NavItemProps) {
+  return (
+    <li className='relative overflow-hidden '>
+      <NavLink
+        to={item.path}
+        className={({ isActive }) =>
+          twMerge(
+            "after:block after:w-full after:h-1 after:bg-hrnet-green after:translate-x-negative-full after:transition-transform after:duration-300",
+            isActive && "after:translate-x-0"
+          )
+        }
+      >
+        {item.label}
+      </NavLink>
+    </li>
+  );
+}
+
+export default NavItem;
