93599ab6f46d8a214a83c5726ddfd882105edb7e
drsky <contact@drsky.ovh>
2024-01-21 12:44:00 +0100

    feat: Implement FormRaw component for responsive grid layout
    - Added FormRaw component with PropsWithChildren type for flexibility.
    - Integrated Tailwind CSS for responsive grid layout.
    - Provided JSDoc comments for clarity and documentation.

--- src/Components/FormRaw/formRaw.tsx
+++ src/Components/FormRaw/formRaw.tsx
@@ -5,11 +5,32 @@ type FormRawProps = PropsWithChildren<{
   className?: string;
 }>;
 
+/**
+ * `FormRaw` is a flexible grid component designed for creating forms with responsive layout.
+ * It automatically adjusts to a two-column layout on desktop and a single-column layout on mobile devices.
+ *
+ * Props:
+ * - `className` (string, optional): A custom CSS class for overriding the default styles of the grid.
+ *    This class will be merged with the default grid classes.
+ * - `children` (React.ReactNode): The content to be rendered within the grid.
+ *
+ * Example usage:
+ * ```
+ * <FormRaw className="my-custom-class">
+ *   <input type="text" placeholder="First Name" />
+ *   <input type="text" placeholder="Last Name" />
+ * </FormRaw>
+ * ```
+ *
+ * @param {FormRawProps} props - The props for the FormRaw component.
+ * @returns {React.Component} The FormRaw component with a grid layout.
+ */
 function FormRaw({ className, children }: FormRawProps) {
   return (
     <div
+      data-testid='form-raw'
       className={twMerge(
-        "grid px-4 lg:px-8 grid-rows-2 grid-cols-1 md:grid-rows-1 md:grid-cols-2 justify-start gap-8",
+        "grid px-4 lg:px-8  justify-start gap-8 grid-rows-2 grid-cols-1 md:grid-rows-1 md:grid-cols-2",
         className
       )}
     >
