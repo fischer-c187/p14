992d5931a994aa9342a003852a768e781fc87847
drsky <contact@drsky.ovh>
2024-01-22 00:59:52 +0100

    feat: implement useFormValidation hook for form validation management
    The useFormValidation hook provides a robust solution for managing form validations. This hook facilitates the handling of input validations, error messages, and form submission status, streamlining the form handling process.

---
+++ src/hooks/useFormValidation.test.tsx
@@ -0,0 +1,140 @@
+import {
+  act,
+  fireEvent,
+  render,
+  renderHook,
+  screen,
+} from "@testing-library/react";
+// import { act } from "react-dom/test-utils";
+
+import { FocusEvent } from "react";
+import { describe, expect, it, vi } from "vitest";
+import useFormValidation from "./useFormValidation";
+import ValidatorsType from "../Types/form";
+
+const validators = {
+  test: {
+    validator: (value: string) => value.length > 0,
+    failMessage: "Name is required",
+  },
+};
+
+const validatorsWithError = {
+  test2: {
+    validator: (value: string) => value.length > 0,
+    failMessage: "Name is required",
+  },
+};
+
+type TestComponentProps = {
+  validatorsObj?: ValidatorsType;
+};
+
+function TestComponent({ validatorsObj = validators }: TestComponentProps) {
+  const { register, errors, validateForm } = useFormValidation(validatorsObj);
+
+  function onSubmit(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    validateForm();
+  }
+
+  return (
+    <form data-testid='form' onSubmit={onSubmit}>
+      <input
+        {...register("test")}
+        type='text'
+        name='test'
+        data-testid='nameInput'
+      />
+      {errors.test?.message && (
+        <span data-testid='nameError'>{errors.test?.message}</span>
+      )}
+    </form>
+  );
+}
+
+describe("useFormValidation", () => {
+  it("should render error when input validator fail", () => {
+    render(<TestComponent />);
+    const input = screen.getByTestId("nameInput");
+    fireEvent.blur(input);
+    expect(screen.getByTestId("nameError")).toHaveTextContent(
+      "Name is required"
+    );
+  });
+
+  it("should not render error when input validator pass", () => {
+    render(<TestComponent />);
+    const input = screen.getByTestId("nameInput");
+    fireEvent.change(input, { target: { value: "test" } });
+    fireEvent.blur(input);
+    expect(screen.queryByTestId("nameError")).not.toBeInTheDocument();
+  });
+
+  it("should not render error when input validator pass and input is not touched", () => {
+    render(<TestComponent />);
+    expect(screen.queryByTestId("nameError")).not.toBeInTheDocument();
+  });
+
+  it("should not render error when input validator pass and form is submitted", () => {
+    render(<TestComponent />);
+    const input = screen.getByTestId("nameInput");
+    fireEvent.change(input, { target: { value: "test" } });
+    fireEvent.submit(screen.getByTestId("form"));
+    expect(screen.queryByTestId("nameError")).not.toBeInTheDocument();
+  });
+
+  it("should render error when input validator fail and form is submitted", () => {
+    render(<TestComponent />);
+    fireEvent.submit(screen.getByTestId("form"));
+    expect(screen.getByTestId("nameError")).toHaveTextContent(
+      "Name is required"
+    );
+  });
+
+  it("should throw an error when we call register with an input name that does not exist in the validators object", () => {
+    const consoleSpy = vi.spyOn(console, "error");
+    consoleSpy.mockImplementation(() => {});
+
+    expect(() =>
+      render(<TestComponent validatorsObj={validatorsWithError} />)
+    ).toThrowError();
+
+    consoleSpy.mockRestore();
+  });
+
+  it('should set validationMode to "onBlur" when onSubmitOnly is false and mode is not defined', () => {
+    const validatorsSubmit = {
+      testField: {
+        validator: () => true,
+        failMessage: "Error",
+        onSubmitOnly: true,
+      },
+    };
+
+    const { result } = renderHook(() => useFormValidation(validatorsSubmit));
+    const inputProps = result.current.register("testField");
+
+    expect(inputProps).not.toHaveProperty("onBlur");
+  });
+
+  it("should not update errors state if the error message is the same", () => {
+    const { result } = renderHook(() => useFormValidation(validators));
+    const firstEvent: FocusEvent<HTMLInputElement, Element> = {
+      // @ts-ignore
+      target: { name: "test", value: "" },
+    };
+    act(() => {
+      result.current.validateOnEvent(firstEvent);
+    });
+    const initialError = result.current.errors.test;
+    expect(initialError).toBeDefined();
+
+    act(() => {
+      result.current.validateOnEvent(firstEvent);
+    });
+
+    const sameError = result.current.errors.test;
+    expect(sameError).toBe(initialError);
+  });
+});
---
+++ src/hooks/useFormValidation.ts
@@ -0,0 +1,86 @@
+import { useRef, useCallback, useState } from "react";
+import ValidatorsType from "../Types/form";
+
+type RefsObject = {
+  [key: string]: HTMLInputElement;
+};
+
+type ErrorsObject = {
+  [key: string]: { message: string };
+};
+
+function useFormValidation(validators: ValidatorsType) {
+  const refs = useRef<RefsObject>({}).current;
+  const [errors, setErrors] = useState<ErrorsObject>({});
+
+  const validateField = useCallback(
+    (name: string, value: string) => {
+      if (Object.prototype.hasOwnProperty.call(validators, name)) {
+        return validators[name].validator(value)
+          ? ""
+          : validators[name].failMessage;
+      }
+      return "";
+    },
+    [validators]
+  );
+
+  const validateOnEvent = useCallback(
+    (event: React.FocusEvent<HTMLInputElement>) => {
+      const { name, value } = event.target;
+      const errorMessage = validateField(name, value);
+
+      setErrors((prevErrors) => {
+        if (prevErrors[name]?.message === errorMessage) {
+          return prevErrors;
+        }
+        return {
+          ...prevErrors,
+          [name]: { message: errorMessage },
+        };
+      });
+    },
+    [validateField]
+  );
+
+  const validateForm = () => {
+    const newErrors = Object.entries(refs).reduce(
+      (acc: ErrorsObject, [name, element]) => {
+        acc[name] = {
+          message: validateField(name, (element as HTMLInputElement).value),
+        };
+        return acc;
+      },
+      {}
+    );
+    setErrors(newErrors);
+  };
+
+  const setRef = useCallback(
+    (name: string) => (element: HTMLInputElement) => {
+      refs[name] = element;
+    },
+    [refs]
+  );
+
+  const register = useCallback(
+    (name: string) => {
+      if (!(name in validators)) {
+        throw new Error(`Validator for ${name} not found`);
+      }
+      const validationMode = validators[name].onSubmitOnly
+        ? undefined
+        : validators[name].mode || "onBlur";
+
+      return {
+        ...(validationMode ? { [validationMode]: validateOnEvent } : {}),
+        ref: setRef(name),
+      };
+    },
+    [setRef, validateOnEvent, validators]
+  );
+
+  return { validateForm, errors, register, refs, validateOnEvent };
+}
+
+export default useFormValidation;
