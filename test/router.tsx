6d1ce13a9969a9cc2f0102d1a861508ef4570870
drsky <contact@drsky.ovh>
2024-01-24 00:00:14 +0100

    chore: add queryClient in renderRoute fonction

--- test/router.tsx
+++ test/router.tsx
@@ -4,15 +4,26 @@ import {
   createBrowserRouter,
 } from "react-router-dom";
 import { render } from "@testing-library/react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 import routes from "../src/routes";
 
+const queryClient = new QueryClient();
+
 export function renderWithRouterContext(element: React.ReactElement) {
-  render(<BrowserRouter>{element}</BrowserRouter>);
+  render(
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>{element}</BrowserRouter>
+    </QueryClientProvider>
+  );
 }
 
 export function renderRoute(route = "/") {
   window.history.pushState({}, "Test Page", route);
 
-  return render(<RouterProvider router={createBrowserRouter(routes)} />);
+  return render(
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={createBrowserRouter(routes)} />
+    </QueryClientProvider>
+  );
 }
