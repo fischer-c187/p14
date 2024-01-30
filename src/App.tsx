c51a0042568a2d9807413aa5917cb8677734486e
drsky <contact@drsky.ovh>
2024-01-30 12:00:58 +0100

    feat: add QueryClient and wrap Router with QueryClientProvider

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
-
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import routes from "./routes";
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+    },
+  },
+});
+
 function App() {
-  return <RouterProvider router={createBrowserRouter(routes)} />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={createBrowserRouter(routes)} />
+    </QueryClientProvider>
+  );
 }
 
 export default App;
