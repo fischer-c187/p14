1727ba9f4d3faa01337fdb01e70bbe566edd1f02
drsky <contact@drsky.ovh>
2024-01-30 12:04:53 +0100

    refact: add sort entry

--- src/constants/tableDescriptor.ts
+++ src/constants/tableDescriptor.ts
@@ -5,26 +5,31 @@ const employeesTableDescriptor: TableDescriptor<Employee> = [
   {
     columnLabel: "First Name",
     accessor: (item: Employee) => item.firstName,
+    sort: "firstName",
     id: 1,
   },
   {
     columnLabel: "Last Name",
     accessor: (item: Employee) => item.lastName,
+    sort: "lastName",
     id: 2,
   },
   {
     columnLabel: "Start Date",
     accessor: (item: Employee) => item.startDate,
+    sort: "startDate",
     id: 3,
   },
   {
     columnLabel: "Department",
     accessor: (item: Employee) => item.department,
+    sort: "department",
     id: 4,
   },
   {
     columnLabel: "Date of Birth",
     accessor: (item: Employee) => item.dateBirth,
+    sort: "dateBirth",
     id: 5,
   },
   {
@@ -35,16 +40,19 @@ const employeesTableDescriptor: TableDescriptor<Employee> = [
   {
     columnLabel: "City",
     accessor: (item: Employee) => item.city,
+    sort: "city",
     id: 7,
   },
   {
     columnLabel: "State",
     accessor: (item: Employee) => item.state,
+    sort: "state",
     id: 8,
   },
   {
     columnLabel: "Zip Code",
     accessor: (item: Employee) => item.zipCode,
+    sort: "zipCode",
     id: 9,
   },
 ];
