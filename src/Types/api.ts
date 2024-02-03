d49c363e2e8cf78851e2770bf553d78eddda35a6
drsky <contact@drsky.ovh>
2024-02-03 19:14:53 +0100

    feat: add function to fetch and post employee data

--- src/Types/api.ts
+++ src/Types/api.ts
@@ -2,7 +2,19 @@ export type Employee = {
   _id: string;
   firstName: string;
   lastName: string;
-  dateBirth: string;
+  birthDate: string;
+  startDate: string;
+  street: string;
+  city: string;
+  state: string;
+  zipCode: string;
+  department: string;
+};
+
+export type EmployeeForm = {
+  firstName: string;
+  lastName: string;
+  birthDate: string;
   startDate: string;
   street: string;
   city: string;
