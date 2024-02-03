31d513f9d2eada474a6147c2c1545b7c49afce59
drsky <contact@drsky.ovh>
2024-02-03 19:20:34 +0100

    feat(Home): add modal functionality for submission feedback

--- src/Pages/home/home.tsx
+++ src/Pages/home/home.tsx
@@ -1,6 +1,23 @@
+import Modal from "@components/Modal/modal";
+import ErrorModalMessage from "@components/ModalMessage/errorModalMessage";
+import SuccessModalMessage from "@components/ModalMessage/successModalMessage";
 import AddEmployee from "@layouts/forms/addEmployee";
+import { useCallback, useState } from "react";
 
 function Home() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [modalType, setModalType] = useState<"success" | "error" | "">("");
+
+  const handleSuccessSubmit = useCallback(() => {
+    setModalType("success");
+    setIsModalOpen(true);
+  }, []);
+
+  const handleErrorSubmit = useCallback(() => {
+    setModalType("error");
+    setIsModalOpen(true);
+  }, []);
+
   return (
     <div className='flex flex-col gap-16 max-w-7xl m-auto py-9 justify-center'>
       <h1
@@ -9,7 +26,17 @@ function Home() {
       >
         Create Employee
       </h1>
-      <AddEmployee />
+      <Modal isOpen={isModalOpen} onClose={() => setIsModalOpen(false)}>
+        {modalType === "success" ? (
+          <SuccessModalMessage />
+        ) : (
+          <ErrorModalMessage />
+        )}
+      </Modal>
+      <AddEmployee
+        onSuccessfulSubmission={handleSuccessSubmit}
+        onFailedSubmission={handleErrorSubmit}
+      />
     </div>
   );
 }
