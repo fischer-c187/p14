4d55d3ebd10c2c572d28c375cd591a417536c071
drsky <contact@drsky.ovh>
2024-02-06 16:22:07 +0100

    feat: implement 404 not found

--- src/Pages/PageNotFound/pageNotFound.test.tsx
+++ src/Pages/PageNotFound/pageNotFound.test.tsx
@@ -1,11 +1,11 @@
-import { render, screen } from "@testing-library/react";
+import { screen } from "@testing-library/react";
 import { expect, describe, it } from "vitest";
 import PageNotFound from "./PageNotFound";
-import { renderRoute } from "../../../test/router";
+import { renderRoute, renderWithRouterContext } from "../../../test/router";
 
 describe("PageNotFound", () => {
   it("should render without crashing", () => {
-    render(<PageNotFound />);
+    renderWithRouterContext(<PageNotFound />);
     expect(screen.getByTestId("pageNotFound")).toBeInTheDocument();
   });
   it("should display page not found page when route is not found", () => {
